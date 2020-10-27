9a96bc41aa8c03bccb1aa516cfb1372fa5386ca3
justin sykes <justin.sykes@ge.com>
2020-10-27 10:20:19 -0400

    updated portfolio page with 4 real projects and added script.js file to enable clickable images

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+console.log("JS works!");
+
+$("#code-quiz").on("click", () => {
+    window.open("https://github.com/204039643/JS-Code-Quiz");
+});
+
+$("#workday-scheduler").on("click", () => {
+    window.open("https://github.com/204039643/my-work-day-scheduler");
+});
+
+$("#weather-dashboard").on("click", () => {
+    window.open("https://github.com/204039643/a-weather-dashboard");
+});
+
+$("#project-one").on("click", () => {
+    window.open("https://github.com/204039643/Senior-Travel-Covid-Care");
+});
