4835129ee21e914abbe36b969454666f5a895627
justin sykes <justin.sykes@ge.com>
2020-10-27 17:03:10 -0400

    updated contact.html page and added new background to all 3 pages

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,33 @@
-console.log("JS works!");
+//JS variables
 
 $("#code-quiz").on("click", () => {
-    window.open("https://github.com/204039643/JS-Code-Quiz");
+    window.open("https://204039643.github.io/JS-Code-Quiz/");
 });
 
 $("#workday-scheduler").on("click", () => {
-    window.open("https://github.com/204039643/my-work-day-scheduler");
+    window.open("https://204039643.github.io/my-work-day-scheduler");
 });
 
 $("#weather-dashboard").on("click", () => {
-    window.open("https://github.com/204039643/a-weather-dashboard");
+    window.open("https://204039643.github.io/a-weather-dashboard");
 });
 
 $("#project-one").on("click", () => {
-    window.open("https://github.com/204039643/Senior-Travel-Covid-Care");
+    window.open("https://204039643.github.io/Senior-Travel-Covid-Care/");
+});
+
+$("#git-hub").on("click", () => {
+    window.open("https://github.com/204039643");
+});
+
+$("#linked-in").on("click", () => {
+    window.open("https://www.linkedin.com/in/justin-sykes-csm-cspo-36819658/");
+});
+
+$("#resume").on("click", () => {
+    window.open("https://docs.google.com/document/d/1PZ16uxr2sGIrMMszYKknz9D2R-Q3Y4qD0JsLetXUahQ/edit?usp=sharing");
+});
+
+$("#submit-btn").on("click", () => {
+    alert(`Thanks, email sent to atlsykes1@att.net!`);
 });
